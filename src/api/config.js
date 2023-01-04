aec74b9fe2fee50d2e5cbde03e04e0562c17b98f
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2023-01-04 22:49:29 +0500

    BASE URL change commit

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-export const BASE_URL = "https://turtkul.kar24.uz/";
+export const BASE_URL = "https://pn.tortkol.uz";
 
 export default axios.create({
   baseURL: BASE_URL,
 });
+
+//https://turtkul.kar24.uz/
